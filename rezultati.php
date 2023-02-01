1fc1393a95b45dc1ba8007e549e721b829df971e
Dimitrije Dobrijevic <dimitrije.dobrijevic@gmail.com>
2023-02-01 19:45:53 +0100

    Dodata je AJAX funkcionalnost za sortiranje tabele

--- rezultati.php
+++ rezultati.php
@@ -15,26 +15,60 @@
     <div>
         <h2>Rezultati utakmica:</h2>
     </div>
-    <div>
+    <div class="rezultati">
         <?php
             include "models/Rezultat.php";
             include "models/Tim.php";
             
             $data = Rezultat::returnAllData();
             foreach($data as $rezultat){
-                $timPrvi = Tim::returnThisTeam($rezultat->getPrviTimId());
-                $timDrugi = Tim::returnThisTeam($rezultat->getDrugiTimId());
+                $timPrvi = Tim::returnTeamById($rezultat->getPrviTimId());
+                $timDrugi = Tim::returnTeamById($rezultat->getDrugiTimId());
                 echo "<div class='rezultat'>";
-                echo "<form method='post'>";
-                echo "<div>" . $rezultat->getDatum() . "</div>";
+                echo "<form action='rezultati.php' method='post'>";
+                echo "<div><input type='date' name='datum' value='" . $rezultat->getDatum() . "' readonly></div>";
                 echo "<div>";
-                echo "<span>" . $timPrvi->getIme() . ": </span><span>" . $rezultat->getPrviTimSetova() . "</span>";
+                echo "<span><input type='text' name='prvi_tim' value='" . $timPrvi->getIme() . "' readonly>: </span><span><input type='number' name = 'prviSetova' value='" . $rezultat->getPrviTimSetova() . "' readonly></span>";
                 echo "</div>";
                 echo "<div>";
-                echo "<span>" . $timDrugi->getIme() . ": </span><span>" . $rezultat->getDrugiTimSetova() . "</span>";
+                echo "<span><input type='text' name='drugi_tim' value='" . $timDrugi->getIme() . "' readonly>: </span><span><input type='number' name = 'drugiSetova' value='" . $rezultat->getDrugiTimSetova() . "' readonly></span>";
+                echo "</div>";
+                echo "<div>";
+                echo "<input type='submit' name='obrisi' value='Obrisi rezultat'>";
                 echo "</div>";
                 echo "</form>";
                 echo "</div>";
+            }
+
+            if(isset($_POST['obrisi'])){
+                $imePrvog = $_POST['prvi_tim'];
+                $imeDrugog = $_POST['drugi_tim'];
+                $prviSetova = $_POST['prviSetova'];
+                $drugiSetova = $_POST['drugiSetova'];
+                $datum = $_POST['datum'];
+                $prviTim = Tim::returnTeamByName($imePrvog);
+                $drugiTim = Tim::returnTeamByName($imeDrugog);
+                if ($prviSetova == 2) {
+                    if ($drugiSetova == 0) {
+                        $prviTim->obrisiPobeda20();
+                        $drugiTim->obrisiPoraz02();
+                    }
+                    if ($drugiSetova == 1) {
+                        $prviTim->obrisiPobeda21();
+                        $drugiTim->obrisiPoraz12();
+                    }
+                } else if ($prviSetova == 1) {
+                    $prviTim->obrisiPoraz12();
+                    $drugiTim->obrisiPobeda21();
+                } else {
+                    $prviTim->obrisiPoraz02();
+                    $drugiTim->obrisiPobeda20();
+                }
+                $prviTim->updateTim();
+                $drugiTim->updateTim();
+                Rezultat::deleteResult($prviTim->getTimId(), $drugiTim->getTimId(), $datum);
+                echo $imeDrugog, Tim::returnTeamByName($imeDrugog)->getIme(), $prviTim->getTimId(), $drugiTim->getTimId(), $datum;
+                unset($_POST['obrisi']);
             }
         ?>
     </div>
